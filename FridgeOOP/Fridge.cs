57e7a185a5d02f736c890fba57209685e4871d50
ABardot <aabardot5@gmail.com>
2018-01-31 14:08:04 -0500

    First commit

---
+++ FridgeOOP/Fridge.cs
@@ -0,0 +1,50 @@
+﻿namespace FridgeOOP
+{
+    public class Fridge
+    {
+        //Fields
+        private int isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+        //Properties
+        public bool IsClean
+        {
+            get { return this.IsClean; }
+            set { this.IsClean = value; }
+        }
+
+        public string DispenserType
+        {
+            get { return this.DispenserType; }
+        }
+
+        public string FoodAmount
+        {
+            get { return this.FoodAmount; }
+            set { this.FoodAmount = value; }
+        }
+
+        public string BulbsWork
+        {
+            get { return this.BulbsWork; }
+            set { this.BulbsWork = value; }
+        }
+
+        //Constructor
+        public Fridge()
+        {
+            //Default
+        }
+
+        public Fridge(int isClean, string dispenserType, int foodAmount, bool bulbWorks)
+        {
+        }
+
+        //Methods()
+    }
+}
+
+//Get allows to see the value to print to console, see another class, etc
+//Set allows us to set value 
